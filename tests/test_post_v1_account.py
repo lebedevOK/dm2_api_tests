bf96b3805fad6107f886fa71fbf593913bc66f82
lebedevOK <teh161@yandex.ru>
2024-01-15 13:24:56 +0300

    first tests added

--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,15 +1,19 @@
 import requests
 import json
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailhogApi
+
 
 def test_post_v1_account():
+    mailhog = MailhogApi(host='http://5.63.153.31:5025')
     api = DmApiAccount(host='http://5.63.153.31:5051')
     json = {
-        "login": "login1201",
-        "email": "login1201@qw.ru",
-        "password": "login120100"
+        "login": "login1503",
+        "email": "login1503@qw.ru",
+        "password": "login150300"
     }
-    response = api.account.post_v1_account(
-        json=json
-    )
+    response = api.account.post_v1_account(json=json)
+    token = mailhog.get_token_from_last_email()
+    response = api.account.put_v1_account_token(token=token)
     print(response)
+    print(response.json())
--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -1,13 +1,14 @@
 import requests
 import json
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailhogApi
 
 def test_put_v1_account_token():
     api = DmApiAccount(host='http://5.63.153.31:5051')
+    mailhog_api = MailhogApi()  # Создание экземпляра класса MailhogApi
+    token=mailhog_api.get_token_from_last_email()
 
-
-
-    response = api.account.put_v1_account_token(token = '0cebc451-86a5-4677-a733-8b42ebbe3187')
+    response = api.account.put_v1_account_token(token=token)
     print(response)
     print(response.url)
 
